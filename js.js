bb9f5a5a90a22788717f8fc9c6c88cde0033a59a
ravolut1on <ravolution4321@gmail.com>
2026-09-16 12:24:27 +0200

    Add files via upload

---
+++ js.js
@@ -0,0 +1,13 @@
+function isEmpty(obj) {
+    for (let key in obj) {
+        return false;
+    }
+    return true;
+}
+
+let schedule = {};
+alert(isEmpty(schedule));
+
+schedule["8:30"] = "Вставай";
+
+alert(isEmpty(schedule));
